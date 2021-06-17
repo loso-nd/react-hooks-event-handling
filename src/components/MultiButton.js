962ed715fb7261638aa098862f1f3e9e77935969
Nahum <losond8@gmail.com>
2021-06-16 20:47:34 -0600

    Done. Handling Events in React.

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -5,13 +5,16 @@ function MultiButton() {
     console.log(`Button ${number} was clicked`);
   }
 
+  // If we pass in a param to any handler func we must be sure to pass the same param to the handler function w/in the sythetic events call. Invoke lines 11-13 with a param on handleClick(number)
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
 }
 
 export default MultiButton;
+
+//can we invoke the handleClick with a number inside the return or inside the function itself?
